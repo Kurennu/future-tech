2e6922f33b6d27a502370257741ea7936716468e
Kurennu <kurenu66@gmail.com>
2025-03-23 14:26:56 +1000

    add: header

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,3 @@
-function multyply(a, b, ...rest) {
-    return a*b* (rest.length > 0 ? rest.reduce((acc, num) => acc *num,1) : 1);
+import Header from './Header.js'
 
-
-}
-console.log(multyply(2,3,4,5));
+new Header()
